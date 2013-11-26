1bee1197a60778e95dc1f1b24f7152e01cfee697
StephWa <swallick@pdx.edu>
2013-11-25 22:15:00 -0800

    Added Androng Source Files from Eclipse
    Original source code for Androng by Patrick Kalkman available at:
    http://www.codeproject.com/Articles/189515/Androng-a-Pong-clone-for-Android

---
+++ TestPong1/src/com/example/testpong1/DrawableResourceCollection.java
@@ -0,0 +1,14 @@
+package com.example.testpong1;
+
+import android.graphics.drawable.Drawable;
+
+import java.util.LinkedList;
+
+public class DrawableResourceCollection extends LinkedList<Drawable>
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
